ac59cb4a0ac6783905efe5771ac2188384f17fa9
Ayoub Amer <ayouub.ameur@gmail.com>
2025-05-05 15:26:44 +0100

    feat: add default, max, and min period to live constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,9 @@
 pub const MAX_GAS_CALL: u64 = 4294167295;
 pub const MIN_GAS_CALL: u64 = 2100000;
 pub const MAX_GAS_DEPLOYMENT: u64 = 3980167295;
+pub const PERIOD_TO_LIVE_DEFAULT: u64 = 9;
+pub const PERIOD_TO_LIVE_MAX: u64 = 9;
+pub const PERIOD_TO_LIVE_MIN: u64 = 1;
 
 pub enum PublicGRPCURL {
     Mainnet,
