5d1969d8f9cf65f306ffbc249b7498bcce9c978a
Ayoub Amer <ayouub.ameur@gmail.com>
2025-05-05 15:20:58 +0100

    feat: add gas constants for MAX_GAS_CALL, MIN_GAS_CALL, and MAX_GAS_DEPLOYMENT

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,7 @@
+pub const MAX_GAS_CALL: u64 = 4294167295;
+pub const MIN_GAS_CALL: u64 = 2100000;
+pub const MAX_GAS_DEPLOYMENT: u64 = 3980167295;
+
 pub enum PublicGRPCURL {
     Mainnet,
     Buildnet,
@@ -11,4 +15,3 @@ impl PublicGRPCURL {
         }
     }
 }
-
